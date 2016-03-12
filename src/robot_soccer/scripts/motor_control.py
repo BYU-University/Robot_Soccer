013788ad78933b791cdd1ed4f5b02474e0d9cd44
rscheuer <jonnylinuxsuse.rs@gmail.com>
2016-03-11 20:58:10 -0700

    reading1

--- src/robot_soccer/scripts/motor_control.py
+++ src/robot_soccer/scripts/motor_control.py
@@ -761,9 +761,9 @@ def tentative(p,i,d,v):
     speed129 = ReadISpeedM1(129)
     #SetM1VelocityPID(address,p,i,d,qpps)
 
-    SetM1VelocityPID(128,p,i,d,speed128a)
-    SetM2VelocityPID(128,p,i,d,speed128b)
-    SetM1VelocityPID(129,p,i,d,speed129)
+    SetM1VelocityPID(128,p,i,d,speed128a[1])
+    SetM2VelocityPID(128,p,i,d,speed128b[1])
+    SetM1VelocityPID(129,p,i,d,speed129[1])
     print "speed128a: ",speed128a
     print "speed128b: ",speed128b
     print "speed129:", speed129
