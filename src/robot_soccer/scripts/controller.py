520a4c4b4507fa952a2f8a3311af42a5bfed8b6a
ryjanski <ryjanski@gmail.com>
2016-03-15 20:37:01 -0600

    kickTime

---
+++ src/robot_soccer/scripts/controller.py
@@ -0,0 +1,52 @@
+import vect2motors
+import calib
+import pygame
+import motor_control as mc
+
+
+#this code is anm intreface between humans and the robot
+#it will call the motor functions and calibration and allow
+#human input to start and stop the robot
+
+
+go = 0
+pygame.init()
+pygame.display.set_mode((400, 400))
+pygame.key.set_repeat(10, 10)
+
+while go == 0:
+	# wait for keyboard initialization
+    for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+		    sys.exit()
+	    keys=pygame.key.get_pressed()
+
+        # starts robot function
+	    if keys[K_ENTER]:
+		    print('Begin Roboboogie')
+		    go = 1
+
+        # quit the program
+	    if keys[K_ESCAPE]:
+		    print('The robots are dead!')
+		    pygame.quit()
+		    break
+
+while go == 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+	        keys=pygame.key.get_pressed()
+	    if keys[K_ESC]:
+		    print('Roboboogie is over')
+		    go = 0
+            vel.goXYOmegaTheta(0,0,0)
+
+
+        # quit the program
+	    if keys[K_ESCAPE]:
+		    print('The robots are dead!')
+		    pygame.quit()
+		    break
+    # below we put the code in that makes tactical decisions and calls the next move
+
+    mc.getBall(data)
--- src/robot_soccer/scripts/kickTime.py
+++ src/robot_soccer/scripts/kickTime.py
@@ -1,18 +1,18 @@
 from kick import *
-#
-count = 0
-bZ = 10 #buffer zone
-kR = .09 #kick range
-aA = .05 #angular accuracy
-fPS = 20 #fieldx-position to shoot from
-cL = 1000 #count limit to reset
 
-def kickTime(xr, toGoal,xball):
-	if count == 0 and xr > fPs and math.abs(toGoal) < aA and xball < kR:
+count = 0
+bZ = 10      # buffer zone
+kR = .09     # kick range
+aA = .05     # angular accuracy
+fPS = 20     # field x-position to shoot from
+cL = 1000    # count limit to reset
+
+def kickTime(xr, toGoal, xball):
+	if kickTime.count == 0 and xr > fPs and math.abs(toGoal) < aA and xball < kR:
 	    kick()
-	    count += 1
-	elif count == 1000 or xball > bZ+kR:
-	    count = 0
+	    kickTime.count += 1
+	elif kickTime.count == 1000 or xball > bZ+kR:
+	    kickTime.count = 0
 	else:
-	    count += 1
+	    kickTime.count += 1
 
