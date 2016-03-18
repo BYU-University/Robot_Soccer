682cba20061e1c385659defd753885b7cdc26f20
ryjanski <ryjanski@gmail.com>
2016-03-17 21:32:10 -0600

    Ryjan changes

--- src/robot_soccer/scripts/run.py
+++ src/robot_soccer/scripts/run.py
@@ -1,15 +1,15 @@
-from controller import *
-from robot_soccer.msg import convertedCoordinates
-import motor_control
+import math as m
+import plays as p
 
 
-def run():
+def run(data):
     bret = [data.home1_x, data.home1_y, data.home1_theta]
     ball = [data.ball_x, data.ball_y]
-
+    bretToBall = m.sqrt((ball[0]-bret[0])**2+(ball[1]-bret[1])**2)
     # here we will pick tactics and call the functions
-    bretToBall =  m.sqrt((ball[0]-bret[0])**2+(ball[1-bret[])**2)
+
     if bretToBall < .01:
-        goToGoal(bret)
+        p.goToGoal(bret)
+
     else:
-        goToBall(bret, ball)
+        p.goToBall(bret, ball)
