c48731479bd30a40a8303d59124215ae5de46f75
rsguru <jonnylinuxsuse.rs@gmail.com>
2016-03-19 20:06:12 -0600

    tesintg 2robot

--- src/robot_soccer/scripts/strategies.py
+++ src/robot_soccer/scripts/strategies.py
@@ -31,7 +31,7 @@ def strategy_init(data):
 	#   p.goToStartForward(bret)
 
     else:
-        p.goToBall(bret, ball)
+        p.goToBall(jamaine, ball)
         #Sp.goToStartDefender(jamaine)
         #p.defendBall(jamaine,ball)
     #p.getBall(data)
