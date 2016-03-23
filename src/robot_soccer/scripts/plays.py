61c74f14e57a29683353a1bf551627b473b299e3
rsguru <jonnylinuxsuse.rs@gmail.com>
2016-03-23 02:13:34 -0600

    Renato-calibrate1

--- src/robot_soccer/scripts/plays.py
+++ src/robot_soccer/scripts/plays.py
@@ -77,7 +77,7 @@ def goToGoal(data):
     goX = distGoalX - data.home1_x
     goY = distGoaly - data.home1_y
     goTheta = tg - data.home1_theta
-    vel.goXYOmegaTheta(goX,goY,goTheta)
+    vel.goXYOmegaTheta(goX,goY,data.home1_theta)
 
 def goTopoint(x,y,t):
     vel.goXYOmegaTheta(x,y,t)
