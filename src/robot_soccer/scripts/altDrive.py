39211ef82ddb0173db0f8f1a0e70426b82dd0757
ryjanski <ryjanski@gmail.com>
2016-03-28 11:33:28 -0600

    Commented out 'p.getBall(bret,ball,goal)' to try some stuff, just comment out everything after that to restore

--- src/robot_soccer/scripts/altDrive.py
+++ src/robot_soccer/scripts/altDrive.py
@@ -18,21 +18,21 @@ def velDrive(x, y, a_d, r):
 
     # individual motor commands we may need a control gain
     OMEGA = P.M3*rotate*commands
-    print('OMEGA 1')
+    print('OMEGA')
     print(OMEGA)
     # use the trims to correct the motor operation
     #OMEGA = np.dot(OMEGA,trims)
 
-    OMEGA[0] = int(round(OMEGA[0] * 1000))
-    OMEGA[1] = int(round(OMEGA[1] * 1000))
-    OMEGA[2] = int(round(OMEGA[2] * 1000))
-    print('OMEGA 2')
-    print(OMEGA)
+    intmega = np.matrix([[0], [0], [0]])
+    intmega[0] = int(round(OMEGA[0] * 1000))
+    intmega[1] = int(round(OMEGA[1] * 1000))
+    intmega[2] = int(round(OMEGA[2] * 1000))
+    print('intmega')
+    print(intmega)
 
-
-    DutyM1(128, OMEGA[0])
-    DutyM2(128, OMEGA[1])
-    DutyM1(129, OMEGA[2])
+    DutyM1(128, intmega[0])
+    DutyM2(128, intmega[1])
+    DutyM1(129, intmega[2])
 
 
 def stop():
