17874551c74786d0567d1c5158987454ac5aead2
rsguru <jonnylinuxsuse.rs@gmail.com>
2016-04-01 21:00:01 -0600

    callmethod2

--- src/robot_soccer/scripts/playable.py
+++ src/robot_soccer/scripts/playable.py
@@ -281,8 +281,8 @@ class playable:
 
 
     def commandRoboclaws(self):
-        correctX = float(-self.vel_x)
-        correctY = float(-self.vel_y)
+        correctX = float(self.vel_x)
+        correctY = float(self.vel_y)
 
         print "values of vel_x,vel_y,Omega,Theta: ", correctX, correctY, self.omega, self.robotHome1.theta
         velchangers.goXYOmegaTheta(correctX, correctY, self.omega, self.robotHome1.theta)
