bab85e8ceee83c0dac42b7047f2791861fd07610
rsguru <jonnylinuxsuse.rs@gmail.com>
2016-04-07 22:23:20 -0600

    goStart1

--- src/robot_soccer/scripts/playable.py
+++ src/robot_soccer/scripts/playable.py
@@ -260,10 +260,10 @@ class playable:
     def goStart(self):
         start = 0.45
         print "info for debugg"
-        xposition = float(self.robotHome1.x-start)
+        xposition = float(self.robotHome1.x+start)
     #print "ballx,bally,homex,homey, hometheta",-ball[0],ball[1],bret[0],bret[1],bret[2]
         self.vel_x = xposition
-        self.vel_y = -self.robotHome1.y
+        self.vel_y = self.robotHome1.y
         self.omega = self.robotHome1.theta
 
 
